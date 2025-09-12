d842bfb811b71b8c4f5e42e50d3ff141cfbd0e81
N1XGit <niilo.johannes.nk@gmail.com>
2025-09-12 09:48:30 +0300

    fix

--- script.js
+++ script.js
@@ -9,23 +9,24 @@ const update = function(inputArray) {
 }
 
 const valid = function() {
-    return (!isNaN(inputArray[inputArray.length - 1]) && inputArray.length < 59
+    return (inputArray.length > 0 &&
+        !isNaN(inputArray[inputArray.length - 1]) &&
+        inputArray.length < 31
     );
 };
 
 const lengthCheck = function() {
-    return (inputArray.length < 60);
+    return (inputArray.length < 32);
 };
 
 const c = function() {
     inputArray = [];
     const input = document.querySelector(".numberInput");
     input.textContent = "input";
+    let result = document.querySelector('.result');
+    result.textContent = "result";
 };
 
-const equals = function() {
-    return 0;
-};
 
 const plus = function() {
     if (valid() && lengthCheck()) inputArray.push("+");
@@ -33,7 +34,8 @@ const plus = function() {
 };
 
 const minus = function() {
-    if (valid() && lengthCheck()) inputArray.push("-");
+    if (inputArray[inputArray.length - 1] != "-" &&
+        inputArray.length < 31) inputArray.push("-");
     update(inputArray);
 };
 
@@ -50,4 +52,67 @@ const divide = function() {
 const passNumber = function(number) {
     if (lengthCheck()) inputArray.push(number);
     update(inputArray);
+};
+
+
+const inputToResult = function() {
+
+    let resultArray = new Array();
+
+    let stringHelper = "";
+
+    for (let i = 0; i < inputArray.length; i++) {
+        
+        if (inputArray[i] == "*" ||
+            inputArray[i] == "/" ||
+            inputArray[i] == "+" ) {
+                
+                resultArray.push(inputArray[i]);
+
+        } else {
+
+            if (inputArray[i] == "-" && !isNaN(inputArray[i - 1])) resultArray.push("+");
+
+            stringHelper += inputArray[i];
+
+            if (isNaN(inputArray[i + 1]) || i === inputArray.length - 1) {
+                        
+                resultArray.push(Number(stringHelper))
+                stringHelper = "";
+
+            };
+        };
+
+    };
+
+    return resultArray;
+};
+
+const equals = function() {
+
+    let array = inputToResult();
+    let result = document.querySelector('.result');
+    let r = 0;
+    let max = 0;
+
+    
+    for (let i = 0; i < array.length; i++) {
+        if (String(array[i]).length > max) max = String(array[i]).length;
+        if (array[i] === "*") {
+            array.splice(i - 1, 3, array[i - 1] * array[i + 1]);
+            i--;
+        } else if (array[i] === "/") {
+            if (array[i + 1] === 0) {
+                result.textContent = "Syntax Error, division by 0 not allowed";
+                return 0;
+            };
+            array.splice(i - 1, 3, array[i - 1] / array[i + 1]);
+            i--
+        };
+    }
+    for (let j = 0; j < array.length; j++) {
+        if (!isNaN(array[j])) r += array[j];
+    }
+    result.textContent = parseFloat(Number(r).toFixed(max));
+    inputArray = [];
 }
